58aa1ee9de95455612f9fa9f794e6dd5bfd5f473
Polly <patdaik@gmail.com>
2017-08-21 11:58:26 -0400

    Update 300_Longest_Increasing_Subsequence.cpp

--- 300_Longest_Increasing_Subsequence.cpp
+++ 300_Longest_Increasing_Subsequence.cpp
@@ -1,1 +1,27 @@
-
+class Solution {
+    public:
+    int lengthOfLIS(vector<int>& nums) {
+        int max = 1;
+        int size = nums.size();
+        int *lengthArray = new int[size];
+        
+        for (int i = 0; i < size; i++)
+        {
+            lengthArray[i] = 1;
+            for (int j = 0; j < i; j++)
+            {
+                if(nums[j] <= nums[i] && lengthArray[j] + 1 > lengthArray[i] )
+                {
+                    lengthArray[i] = lengthArray[j] + 1;
+                }
+                if (lengthArray[i] > max) 
+                {
+                    max = lengthArray[i];
+                }
+            }
+        }
+        delete[] lengthArray;
+        
+        return max;
+    }
+};
