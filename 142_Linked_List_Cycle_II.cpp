f6128896cbd6f54f8734cb2149ad4758f6c669dc
Polly <patdaik@gmail.com>
2017-10-15 18:54:11 -0400

    Create 142_Linked_List_Cycle_II.cpp

---
+++ 142_Linked_List_Cycle_II.cpp
@@ -0,0 +1,25 @@
+/*
+Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+Follow up:
+Can you solve it without using extra space? 
+*/
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) break;
+        }
+        if (!fast || !fast->next) return NULL;
+        slow = head;
+        while (slow != fast) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        return fast;
+    }
+};
